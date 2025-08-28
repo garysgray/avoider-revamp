2ef9ea2348672fd8aa685e97120e5233909ba98d
garysgray <garys.gray@gmail.com>
2025-08-27 20:16:51 -0700

    changes to render text getting rid of magic numbers also cleaned up functions in helpers thta were elated to text rendering changes

--- Avoider-Game-HTML5-master/render/textRenderer.js
+++ Avoider-Game-HTML5-master/render/textRenderer.js
@@ -3,9 +3,24 @@
 //no game logic should be called in here some for placment of objects maybe
 //called from controller in the update function
 // gotta fix some magic numbers
-function renderText(aDev,aGame)
+
+
+const layout = {
+    initTextY: [0.7, 0.75, 0.8],   // percentages from top for INIT instructions
+    hudY: 0.95,                     // 5% from top for HUD
+    hudAmmoX: 0.05,                  // 5% from left
+    hudLivesX: 0.85,                 // 85% from left
+    pauseY: 0.65,                    // 75% from top
+    winLoseY: 0.65                   // same as pause for WIN/LOSE
+};
+
+function renderText(aDev, aGame)
 {
-    aDev.setFont("bold 14pt Calibri");
+    const cw = aDev.canvas.width;
+    const ch = aDev.canvas.height;
+    const fontSize = 16; // in pts
+
+    aDev.setFont(`bold ${fontSize}pt Calibri`);
     aDev.colorText("white");
     
     //use game state to dictate what should text should render
@@ -14,45 +29,46 @@ function renderText(aDev,aGame)
     {
         case gameStates.INIT://init-gameState
         {         
-            //text to help player out
-            aDev.centerTextX("CATCH  FIRE  BALLS  TO  GET  AMMO",aDev.canvas.height-150);
-            aDev.centerTextX("USE  SPACE-BAR  TO  FIRE",aDev.canvas.height-100);
-            aDev.centerTextX("PRESS  THE  SPACE-BAR  TO  START",aDev.canvas.height-50);
+           layout.initTextY.forEach((pct, idx) => {
+                const msg = [
+                    "CATCH FIRE BALLS TO GET AMMO",
+                    "USE SPACE-BAR TO FIRE",
+                    "PRESS THE SPACE-BAR TO START"
+                ][idx];
+                aDev.centerTextOnY(msg, ch * pct);
+            });
+            break;
         }
         break;
         
         case gameStates.PLAY://play-gameState	
         {
-            aDev.colorText("red");//changes color of font until its changed           
-            ////HUD            
-            aDev.centerTextX("Score :  "+ aGame.score.toString(),aDev.canvas.height-25);
-            aDev.putText("Ammo :  "+ aGame.ammo.toString(),25,aDev.canvas.height-25);
-            aDev.putText("Lives :  "+ aGame.lives.toString(),500,aDev.canvas.height-25);	  
+            aDev.colorText("red");
+            aDev.centerTextOnY("Score: " + aGame.score, ch * layout.hudY);
+            aDev.putText("Ammo: " + aGame.ammo, cw * layout.hudAmmoX, ch * layout.hudY);
+            aDev.putText("Lives: " + aGame.lives, cw * layout.hudLivesX, ch * layout.hudY);	  
         }
         break;
 
-        case gameStates.PAUSE://pause-state
-        {
-            aDev.colorText("white");//font will be this color untill changed
-            //text to help player out
-            aDev.centerTextX("PRESS  P  TO  RESUME  GAME",aDev.canvas.width/4,aDev.canvas.height-50);
-        }
-        break;
-        case gameStates.WIN://Win-gameState
-        {
-            aDev.centerTextX("PRESS  THE  R  KEY  TO  PLAY",aDev.canvas.width/4,aDev.canvas.height-50);
-        }
-        break;
+        case gameStates.PAUSE:
+            aDev.colorText("white");
+            aDev.centerTextOnY("PRESS P TO RESUME GAME", ch * layout.pauseY);
+            break;
+
+        case gameStates.WIN:
+            aDev.centerTextOnY("PRESS R TO PLAY AGAIN", ch * layout.winLoseY);
+            break;
+
         //die or game over text
         case gameStates.LOSE://Lose-gameState
         {	            
             if(aGame.lives <= 0)
             {				
-                aDev.centerTextX("SORRY  YOU  LOST,  PRESS  R  TO  RETRY",aDev.canvas.width/4,aDev.canvas.height-50);
+                aDev.centerTextOnY("SORRY  YOU  LOST,  PRESS  R  TO  RETRY", ch * layout.winLoseY);
             }
             else
             {
-                aDev.centerTextX("SORRY  YOU  DIED,  PRESS  R  TO  REVIVE",aDev.canvas.width/4,aDev.canvas.height-50);
+                aDev.centerTextOnY("SORRY  YOU  DIED,  PRESS  R  TO  REVIVE", ch * layout.winLoseY);
             }            
         }
         break;
